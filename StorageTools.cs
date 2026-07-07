2076c46c257de249cccd9e57fcdb1e1ea4eae373
Arlo <arlo.godfrey@outlook.com>
2026-07-07 13:50:43 -0500

    Reinforce `://` protocol scheme requirement

--- StorageTools.cs
+++ StorageTools.cs
@@ -1306,7 +1306,7 @@ public static class StorageTools
         }
     }
 
-    [Description("Lists all currently mounted folders and their information. Mounted folder protocol schemas are IDs that act as direct alias substitutes for other folder IDs, or file IDs in the case of archive folders. Use these mount `protocolScheme://` IDs as starting points for GetItemByRelativePath navigation.")]
+    [Description("Lists all currently mounted folders and their information. Mounted folder `protocolScheme://`s are IDs that act as direct alias substitutes for other folder IDs, or file IDs in the case of archive folders. Use these mount `protocolScheme://` IDs as starting points for GetItemByRelativePath navigation.")]
     public static async Task<MountedFolderInfo[]> GetMountedFolders()
     {
         try
